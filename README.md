3d922fa7b95bf8c08dad1059009fe3deafe25361
Ibraamaulana <mi7908368@gmail.com>
2025-10-24 14:17:51 +0700

    Sangar

---
+++ README.md
@@ -0,0 +1,16 @@
+## Pengembang
+Nama: Arthur Leywin  
+Proyek: DocumentationPractice
+# Codelab1
+
+## Deskripsi
+Program *Codelab1* dibuat menggunakan bahasa *Java* untuk mencari *nama terpanjang* dari sekumpulan nama yang sudah disediakan di dalam array.  
+Jika terdapat lebih dari satu nama dengan panjang yang sama, semua nama tersebut akan ditampilkan.
+
+## Cara Menjalankan
+1. Buka proyek di *IntelliJ IDEA*.
+2. Jalankan file Codelab1.java.
+3. Program akan menampilkan nama atau nama-nama dengan panjang karakter terbanyak dari daftar yang sudah ada.
+
+## Contoh Output
+Nama terpanjang adalah: [Cahyo, Diana]
---
+++ src/Codelab1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Codelab1 {
+    public static void main(String[] args) {
+        String[] nama = {"Adi", "Budi", "Cahyo", "Diana", "Eva"};
+        ArrayList<String> namaTerpanjang = cariNamaTerpanjang(nama);
+        System.out.println("Nama terpanjang adalah: " + namaTerpanjang);
+    }
+
+    public static ArrayList<String> cariNamaTerpanjang(String[] array) {
+        ArrayList<String> hasil = new ArrayList<>();
+        int maxLength = 0;
+
+        for (String nama : array) {
+            if (nama.length() > maxLength) {
+                maxLength = nama.length();
+                hasil.clear();
+                hasil.add(nama);
+            } else if (nama.length() == maxLength) {
+                hasil.add(nama);
+            }
+        }
+        return hasil;
+    }
+}
